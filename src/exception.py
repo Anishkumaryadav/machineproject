c190561783f25335d297e7b260f0e411b22868af
Anni <2022ume1815@mnit.ac.in>
2025-04-01 06:15:14 +1300

    logger

--- src/exception.py
+++ src/exception.py
@@ -15,12 +15,7 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
-if __name__=='__main__':
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info('Divide by zero')
-        raise CustomException(e,sys)
+
 
 
         
